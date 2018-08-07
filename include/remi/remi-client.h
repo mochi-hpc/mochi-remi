0569bb17861ab0d658cbacb0c0df7af3e2399ed0
Matthieu Dorier <mdorier@anl.gov>
2018-08-07 13:36:18 +0200

    client interface

--- include/remi/remi-client.h
+++ include/remi/remi-client.h
@@ -8,6 +8,24 @@
 extern "C" {
 #endif
 
+typedef struct remi_client* remi_client_t;
+#define REMI_CLIENT_NULL ((remi_client_t)0)
+
+int remi_client_init(margo_instance_id mid, remi_client_t* client);
+
+int remi_client_finalize(remi_client_t client);
+
+int remi_provider_handle_create(
+        remi_client_t client,
+        hg_addr_t addr,
+        uint16_t provider_id,
+        remi_provider_handle_t* handle);
+
+int remi_provider_handle_ref_incr(remi_provider_handle_t handle);
+
+int remi_provider_handle_release(remi_provider_handle_t handle);
+
+int remi_shutdown_service(remi_client_t client, hg_addr_t addr);
 
 #if defined(__cplusplus)
 }
